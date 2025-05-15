debddd0a795a6654b8df63c27a58d443b0fc3725
munibaweb123 <munibavirtualuni@gmail.com>
2025-05-15 07:47:54 +0500

    user input for path in renaming bulk files project 22

--- Project_22/rename.py
+++ Project_22/rename.py
@@ -2,9 +2,10 @@ import os
 
 def main():
     i = 0
-    path = 'C:/Users/Sikandar/Documents/test/'
+    # path = 'C:/Users/Sikandar/Documents/test/'
+    path = input('Enter path to rename the bulk files in folder/directory:')
     for filename in os.listdir(path):
-        my_dest = "img" + str(i) + ".png"
+        my_dest = "image" + str(i) + ".png"
         my_source = path + filename
         my_dest = path + my_dest
         os.rename(my_source, my_dest)
